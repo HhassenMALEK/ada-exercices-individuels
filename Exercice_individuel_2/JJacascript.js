0cca28a53e1cfd91e3c48aed90b9cdd1d5ca1ba6
root <root@DESKTOP-460SSS5>
2024-02-15 15:02:56 +0100

    MAJ2

--- Exercice_individuel_2/JJacascript.js
+++ Exercice_individuel_2/JJacascript.js
@@ -2,12 +2,15 @@
 
 let numberSought= Math.floor(Math.random() * 50);
 function gamePlay(){
-
 let retourDidIWin = false
+let i=0
 while( retourDidIWin == false){
     let givenNumber=prompt("Deviner le numéro caché : ")
     retourDidIWin=didIWin(givenNumber, numberSought)
+    i++
 } 
+console.log(i)
+document.body.innerHTML += "<h1> vous avez joué " + i +" fois pour trouver le numéro caché</h1>"
 }
 //fonction didIWin() contrôle et affiche le numéro entré par l'utilisateur s'il est petit ou grand par rapport au numéro caché
 function didIWin(givenNumber, numberSought){
@@ -25,9 +28,9 @@ function didIWin(givenNumber, numberSought){
         document.body.innerHTML += "<h1> Bravo vous avez deviné le nombre: "+ numberSought +"</h1>"   
         return true
     }
-    }
+}
 
-    //var randomNumber = Math.floor(Math.random() * 51);
+    
  
 
  
