d02500a83e2fa17b69a689ecec54e89a7a959f32
root <root@DESKTOP-460SSS5>
2024-02-23 16:17:03 +0100

    mise à jour + Ajout joueur 2

--- Exercice_individuel_2/JJacascript.js
+++ Exercice_individuel_2/JJacascript.js
@@ -1,17 +1,38 @@
+// un boutton commencer pour lancer un numero aleatoire a deviner
+let button= document.getElementById("button")
+button.addEventListener("click", function(){
+    let numberSought= Math.floor(Math.random() * 50);
+    console.log(numberSought)
+    gamePlay(numberSought)
+})
+// un bouton  pour rafraichir la page du navigateur
+let reloadbutton= document.getElementById("reloadbutton")
+reloadbutton.addEventListener("click", function(){
+    window.location.reload()
+    
+})    
 function gamePlay(numberSought){ 
-    let retourDidIWin = false
+    let retourDidIWin1 = false
+    let retourDidIWin2 = false
     let i=0
-//boucle de prompt pour deviner le numéro seceret   
-while( retourDidIWin == false){
-    let givenNumber=prompt("Deviner le numéro secret: ")
-    retourDidIWin=didIWin(givenNumber, numberSought)
+//boucle de prompt pour deviner le numero seceret   
+while( retourDidIWin1 == false || retourDidIWin2 == false){
     i++
+    let givenNumber1=prompt("joueur 1 : devinez le numero secret: ")
+        retourDidIWin1=didIWin(givenNumber1, numberSought)
+       if (retourDidIWin1== true){
+        break;
+       }
+    let givenNumber2=prompt("joueur 2 : devinez le numero secret: ")
+        retourDidIWin2=didIWin(givenNumber2, numberSought)
+  
+       break 
 } 
-console.log("nombre de jeu" +i)
-let roundsPlayed= "Vous avez joué " + i +" fois pour trouver le numéro secret: " + numberSought
+console.log("nombre de jeu " +i)
+let roundsPlayed= "Vous avez joué " + i +" fois pour trouver le numèro secret: " + numberSought
 document.getElementById("indicateur").innerText += roundsPlayed
 }
-//fonction didIWin() contrôle et affiche le numéro entré par l'utilisateur 
+//fonction didIWin() controle et affiche le numero entre par l'utilisateur 
 function didIWin(givenNumber, numberSought){
     if (givenNumber >numberSought) {
         higerNumber= givenNumber +" est plus grand que le numéro secret"
@@ -24,9 +45,9 @@ function didIWin(givenNumber, numberSought){
         return false;   
     }     
     else {
-        numberFound="Bravo vous avez gagné: "
+        Message="Bravo vous avez gagné: "
         console.log("numéro secret"+ numberSought)  
-       document.getElementById("numeroTrouvee").innerText += numberFound
+       document.getElementById("numeroTrouvee").innerText += Message
         return true
     }
 }
