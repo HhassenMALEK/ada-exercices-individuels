ceef7b7e64fe7137df2214c54f6d3b3fe5b0a396
root <root@DESKTOP-460SSS5>
2024-03-12 22:34:55 +0100

    ajout *la fonction Fibonacci

--- Exercice_individuel5_recursivite/script.js
+++ Exercice_individuel5_recursivite/script.js
@@ -4,60 +4,100 @@ console.log("les valeurs du tableau tabEntier: " +tabEntier);
 console.log("------------------------------------------"); 
 
 function sum1(tableau){
-    let sumTab=0 ; 
+    let result =0 ; 
     // une boucle for ... of
     for (let i of tableau) {
-        sumTab = sumTab + i;
+        result = result + i;
     }
-    return sumTab ; 
+    return result ; 
 }
 // fonction récursive:
 function sum2(tableau){
-    let sumTab = 0 ;
+    let result = 0;
     if(tableau.length === 0 ){
         return 0;
     } else {
 
-    sumTab = tableau[0] + sum2(tableau.slice(1))
-    return  sumTab
+        result = tableau[0] + sum2(tableau.slice(1))
+    return  result 
     }
 }
 console.log("sum1 :" + sum1(tabEntier)) ;
 console.log("------------------------------------------"); 
 
 console.log("sum2 :" + sum2(tabEntier)) ;
-console.log("------------------------------------------");
+console.log("__________________________________________");
 
 
 
 function factoriel1(nbr) {
     if (nbr === 0 || nbr === 1) {
         return 1;
-    }
+    } else {
     let result = 1;
     for (let i = 2; i <= nbr; i++) {
         result = result * i; //result *= i;
     }
-    return result;
+    return result ;
+    }
 }
 
 function factoriel2(nbr) {
-    // Si nbr = 0, le factoriel est 1
     if (nbr === 0) {
         result = 1 ;
     }
-    // Sinon, on retourne nbr multiplie le factoriel de (nbr - 1)
     else {
         result = nbr * factoriel2(nbr - 1);
     }
         
-    return result
+    return result;
 }
 
-let n = 6 ; 
+let n = 11 ; 
 
 console.log("factoriel N°1 " + n + " est " + factoriel1(n)) ;
 console.log("------------------------------------------") ;
 
 
-console.log("factoriel N°2 " + n + " est " + factoriel1(n)) ; 
+console.log("factoriel N°2 " + n + " est " + factoriel1(n)) ;  
+console.log("__________________________________________");
+
+ 
+
+
+//F0 	F1 	F2 	F3 	F4 	F5 	F6 	F7 	F8 	F9 	F10  F11 	F12 	F13 	F14 	F15 	... 	
+//0 	1 	1 	2 	3 	5 	8 	13 	21 	34 	55 	 89 	144 	233 	377 	610 	... 	
+
+
+function fibonacci1(nbr) {
+    if (nbr ===0 || nbr ===1){
+        return nbr; 
+    } else { 
+        let nbr1 = 0;
+        let nbr2 = 1;
+        let result = 0;
+        for(let i = 2; i <= nbr; i++){
+           result = nbr1 + nbr2; 
+           nbr1 = nbr2; 
+           nbr2 = result; 
+        } 
+        return result
+    }    
+}
+ console.log("Fibonacci 1 de " + n + " est " + fibonacci1(n));
+ console.log("------------------------------------------") ;
+
+
+
+ //F(n)= F(n-1) + F(n-2)
+ 
+// fonction récursive:
+function fibonacci2(nbr){
+     if (nbr ===0 || nbr ===1){
+        result = nbr; 
+     } else { 
+       result =  fibonacci2(nbr - 1) + fibonacci2(nbr - 2);
+    }
+    return result; 
+}
+console.log("Fibonacci 2 de " + n + " est " + fibonacci2(n))        
